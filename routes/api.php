4b20def32e4d7e664259b2fccbbaaf6e8031d8e2
kota nishikawa <koonii26@gmail.com>
2021-03-17 11:15:21 +0900

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/user', fn() => Auth::user())->name('user');
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
+Route::get('/photos', 'PhotoController@index')->name('photo.index');
+Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
+Route::post('/photos/{photo}/comments', 'PhotoController@addComment')->name('photo.comment');
